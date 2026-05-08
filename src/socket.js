b1cb2f5e3eb66482cb6fdd791f9bf44aaa3fa90e
Atharv3904 <atharvkulkarni3904@gmail.com>
2026-05-08 14:28:26 +0530

    scoket file changes

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,13 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL);
+const isLocalHost =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1";
+
+const socketUrl = isLocalHost
+  ? "http://localhost:4990"
+  : import.meta.env.VITE_SOCKET_URL;
+
+export const socket = io(socketUrl, {
+  transports: ["websocket", "polling"],
+});
